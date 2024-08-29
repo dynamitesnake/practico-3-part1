860947e1e7e1f95ef51b5d3930f4ba64e8fda511
debiieagomez <deborahgomez71@gmail.com>
2024-08-29 01:38:18 -0300

    clase main y grafica eje2

--- Eje3interfacesgrafica/src/eje3interfacesgrafica/Eje3interfacesgrafica.java
+++ Eje3interfacesgrafica/src/eje3interfacesgrafica/Eje3interfacesgrafica.java
@@ -4,9 +4,10 @@ package eje3interfacesgrafica;
 
 public class Eje3interfacesgrafica {
 
-   
     public static void main(String[] args) {
-       
+       Credenciales creden = new Credenciales();
+       creden.setVisible(true);
+       creden.setLocationRelativeTo(null);
     }
     
 }
